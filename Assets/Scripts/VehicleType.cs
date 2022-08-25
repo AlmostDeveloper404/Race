3fa3281192538fcd4cf6ab2ed6e8b63f86125ef3
AlmostDeveloper404 <vadimonishchyk@yandex.ru>
2022-08-25 15:10:40 +0300

    Confetti

--- Assets/Scripts/VehicleType.cs
+++
@@ -1,9 +0,0 @@
-using System.Collections;
-using System.Collections.Generic;
-using UnityEngine;
-
-public class VehicleType : MonoBehaviour
-{
-    public GameObject DestroedCar;
-    public Customization Customization;
-}
